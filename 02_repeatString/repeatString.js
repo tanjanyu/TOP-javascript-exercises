68f469163b8ac1f1ef8a53dd6f419e7cf12a3e4a
Jan <janyutan@gmail.com>
2024-04-30 21:20:47 +0800

    Complete exercises 01-03

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, repeatNum) {
+    if (repeatNum < 0) {
+        return "ERROR";
+    }
+    let result = "";
+    for (let i = 0; i < repeatNum; i++) {
+        result = result.concat(string);
+    }
+    return result;
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,19 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let stringArray = [];
+    // store the numbers in an array so that can be reveresed into a string later
+    stringLength = string.length;
+    for (let i = 0; i < stringLength; i++) {
+        stringArray.push(string.charAt(i));
+    }
 
+    let reversedResult = "";
+    for (let i = 0; i < stringLength; i++) {
+        reversedResult = reversedResult.concat(stringArray[ (stringLength - 1) - i]);
+    }
+    return reversedResult;
 };
+
+reverseString("Hello");
 
 // Do not edit below this line
 module.exports = reverseString;
