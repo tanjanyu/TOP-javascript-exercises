233a1a41fa38879800aee1edc72713d6459df47a
Jan <janyutan@gmail.com>
2024-04-30 22:53:56 +0800

    Complete exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array) {
+    let arrayLength = array.length;
+    for (let i = 0; i < arguments.length; i++) {
+        for (let j = 0; j < arrayLength; j++) {
+            if (arguments[i] === array[j]) {
+                // Removes the part of the array that corresponds to the argument
+                array.splice(j, 1);
+                j--;
+            }
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
