0fc44dd00cdcbe323b3ae2845f02d6cdb4937360
Jan <janyutan@gmail.com>
2024-05-02 15:21:05 +0800

    Complete exercise 05 sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,14 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    // check if arguments are numbers else return error
+    if ( (typeof(num1) !== "number") || (typeof(num2) !== "number") || (num1 < 0) || (num2 < 0)) {
+        return "ERROR";
+    }
+    // use arithmetic progression sum formula
+    let smallerNum = Math.min(num1, num2);
+    let biggerNum = Math.max(num1, num2);
+    let numOfTerms = biggerNum - smallerNum + 1;
+    let sum = (numOfTerms) * ((smallerNum + biggerNum) / 2);
+    return sum;
 };
-
 // Do not edit below this line
 module.exports = sumAll;
