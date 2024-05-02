91a6e541a670cfd4b540c242d617744d609d0c5b
Jan <janyutan@gmail.com>
2024-05-02 15:48:45 +0800

    Complete 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToFahrenheit = function(celcius) {
+  let fahrenheit = (9/5) * celcius + 32;
+  let roundedFahrenheit = Math.round(fahrenheit * 10) / 10;
+  return roundedFahrenheit;
 };
 
-const convertToFahrenheit = function() {
+const convertToCelsius = function(fahrenheit) {
+  let celcius = (fahrenheit - 32) * (5/9);
+  let roundedCelcius = Math.round(celcius * 10) / 10;
+  return roundedCelcius;
 };
 
 // Do not edit below this line
