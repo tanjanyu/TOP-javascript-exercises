44f2e54fe613983cc5a05802029192a320e9b801
Jan <janyutan@gmail.com>
2024-05-27 16:23:20 +0800

    Complete 09 exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+    let array = word.split("");
+    array = array.filter((char) => (/^[0-9a-z]+$/i.test(char)));
+    for (let i = 0; i < array.length; i++) {
+    if (array[i].toLowerCase() !== array[array.length - 1 - i].toLowerCase()) {
+      return false;
+    }
+  }
+  return true;
 };
 
 // Do not edit below this line
