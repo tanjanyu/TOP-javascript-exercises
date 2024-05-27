b499b7f78e97d9628964326fe55c8abaf986ecff
Jan <janyutan@gmail.com>
2024-05-27 16:54:27 +0800

    Complete 10 exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,19 @@
-const fibonacci = function() {
-
+const fibonacci = function(number) {
+    if (Number(number) === 0) {
+        return 0;
+    }
+    if (Number(number) < 0) {
+        return "OOPS";
+    }
+    let output = 1, prevNum = 1, secPrevNum = 0;
+    for (let i = 1; i < Number(number); i++) {
+        output = prevNum + secPrevNum;
+        secPrevNum = prevNum;
+        prevNum = output;
+    }
+    return output;
 };
+
 
 // Do not edit below this line
 module.exports = fibonacci;
