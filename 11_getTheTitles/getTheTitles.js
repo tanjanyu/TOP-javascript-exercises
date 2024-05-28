6d06b1d1f2946a3e27afce708cc738cf294352ce
Jan <janyutan@gmail.com>
2024-05-28 16:40:51 +0800

    Complete 11 and 12 exercise

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,8 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    return books.reduce((titles, book) => {
+      titles.push(book.title);
+      return titles;
+    }, []);
 };
 
 // Do not edit below this line
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,22 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    return people.reduce((oldestPerson, person) => {
+        let currentOldestAge = 0, thisPersonAge = 0;
+        oldestPerson.yearOfDeath
+        ? currentOldestAge = oldestPerson.yearOfDeath - oldestPerson.yearOfBirth
+        : currentOldestAge = 2024 - oldestPerson.yearOfBirth;
 
-};
+        person.yearOfDeath
+        ? thisPersonAge = person.yearOfDeath - person.yearOfBirth
+        : thisPersonAge = 2024 - person.yearOfBirth;
+
+        if(thisPersonAge > currentOldestAge) {
+          return person;
+        }
+        return oldestPerson;
+    })
+  };
+
+// function getAge()
 
 // Do not edit below this line
 module.exports = findTheOldest;
